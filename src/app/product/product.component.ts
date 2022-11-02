95e624fb72dc9163741267760b9bde7c524205f7
Kevin Daniel <kevin.daniel@capgemini.com>
2022-11-02 12:23:50 +0100

    feat(mock-call): added a mock call with Mockoon to fetch products datas

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from './product.service';
+import { Product } from './product';
 
 @Component({
   selector: 'app-product',
@@ -6,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent implements OnInit {
-  constructor() {}
+  public products: Product[] | undefined;
 
-  ngOnInit(): void {}
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe((res) => {
+      this.products = res;
+    });
+  }
 }
